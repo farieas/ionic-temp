543d7516e45e152c7d4b66699c48856422cb3a51
farhankdi <43706147+farieas@users.noreply.github.com>
2025-10-03 20:39:33 +0530

    Clean up idx-template.nix by removing comments
    Removed commented-out code related to project setup.

--- idx-template.nix
+++ idx-template.nix
@@ -8,19 +8,5 @@
     mv "$WS_NAME" "$out"
     chmod -R u+w "$out"
     (cd "$out"; npm install --package-lock-only --ignore-scripts)
-
-   # npx --prefer-offline -y @ionic/cli start "$WS_NAME" blank --type=angular --no-deps --no-git --no-link --no-interactive
-   # mkdir "$WS_NAME"/.idx
-   # cp ${./dev.nix} "$WS_NAME"/.idx/dev.nix && chmod +w "$WS_NAME"/.idx/dev.nix
-   # mv "$WS_NAME" "$out"
-    
-   # mkdir -p "$out/.idx"
-
-   # chmod -R u+w "$out"
-   # cp -rf ${./.idx/airules.md} "$out/.idx/airules.md"
-   # cp -rf "$out/.idx/airules.md" "$out/GEMINI.md"
-   # chmod -R u+w "$out"
-
-   # (cd "$out"; npm install --package-lock-only --ignore-scripts)
   '';
 }
